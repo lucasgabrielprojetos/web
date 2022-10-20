f73e9af1f86c74fadfb4ab82525751d077901ea6
lucasgabrielprojetos <103006053+lucasgabrielprojetos@users.noreply.github.com>
2022-10-19 21:59:39 -0300

    added more things

--- aual de crud/index.js
+++ aual de crud/index.js
@@ -1,5 +1,30 @@
-const botao = document.querySelector("#botao");
+const botao = document.querySelector("#botaoAdicionar");
+botao.addEventListener("click", (e) => {
+    e.preventDefault()
+    let titulo = document.querySelector("#titulo").value
+    let pontos = document.querySelector("#pontos").value
 
-botao.addEventListener("click", () => {
-    
+    let card = document.createElement("div")
+    document.querySelector("#tarefas").appendChild(card)
+    card.classList.add("card")
+
+    let h2 = document.createElement("h2")
+    h2.textContent = titulo
+
+    let span = document.createElement("span")
+    span.textContent = pontos + " pontos"
+
+    let meter = document.createElement("meter")
+    meter.value = pontos
+    meter.min = 0
+    meter.max = 100
+
+    card.appendChild(h2)
+    card.appendChild(span)
+    card.appendChild(meter)
+
+    document.querySelector("#tarefas").appendChild(card)
+
+    console.log(titulo)
+    console.log(pontos)
 })
